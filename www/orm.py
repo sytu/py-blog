6254b51d5a36f0340f6dd2d4e978d2c3f7f82d1c
sytu <imsytu@163.com>
2017-03-29 19:22:21 +0800

    up

--- www/orm.py
+++ www/orm.py
@@ -128,9 +128,6 @@ class BooleanField(Field):
     def __init__(self, name=None, default=False):  
         super().__init__(name,'Boolean',False, default)  
 # 不知道这个column type是否可以自己定义 先自己定义看一下  
-class IntegerField(Field):  
-    def __init__(self, name=None, primary_key=False, default=0):  
-        super().__init__(name, 'int', primary_key, default)  
 class FloatField(Field):  
     def __init__(self, name=None, primary_key=False,default=0.0):  
         super().__init__(name, 'float', primary_key, default)  
@@ -348,55 +345,3 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:  
             logging.warning('failed to delete by primary key: affected rows: %s' %rows)  
 
-# from orm import Model, StringField, IntegerField
-if __name__=="__main__": # 测试orm
-#一个类自带前后都有双下划线的方法，在子类继承该类的时候，这些方法会自动调用，比如__init__  
-    # 定义一个User类 User对象映射users表中的一行, 表示一个用户
-    class User(Model): #虽然User类乍看没有参数传入，但实际上，User类继承Model类，Model类又继承dict类，所以User类的实例可以传入关键字参数  
-        # 定义类的属性到列的映射：
-        # __table__, id, name, email, password为三个类属性, 用来描述User对象和表的映射关系
-        __table__ = 'users'  
-        id = IntegerField('id',primary_key=True) #主键为id， tableName为User，即类名  
-        name = StringField('name')  
-        email = StringField('email')  
-        password = StringField('password')  
-    # 创建实例例子:
-    # user = User(id=123, name='Michael')
-    # 存入数据库:
-    # user.insert()
-    # 查询所有User对象:
-    # users = User.findAll()
-
-    #创建异步事件的句柄  
-    loop = asyncio.get_event_loop()  
-   
-    #创建实例    
-    async def test():  
-        await create_pool(loop=loop, host='localhost', port=3306, user='root', password='28', db='test')  
-        user = User(id=3, name='mary', email='759@gmail.com', password='12345')  
-        r = await User.findAll()  
-        print(r)  
-        await user.save()  
-        #ield from user.update()  
-        #yield from user.delete()  
-        # r = yield from User.find(8)  
-        # print(r)  
-        # r = yield from User.findAll()  
-        # print(1, r)  
-        # r = yield from User.findAll(name='sly')  
-        # print(2, r)  
-        await destroy_pool()  #关闭pool  
-   
-    loop.run_until_complete(test())  
-    loop.close()  
-    if loop.is_closed():  
-        sys.exit(0) 
-
-# mysql> create database test;
-# mysql> use test
-# mysql> create table users (
-# -> id bigint,
-# -> name varchar(100),
-# -> email varchar(100),
-# -> password varchar(100)
-# -> );
